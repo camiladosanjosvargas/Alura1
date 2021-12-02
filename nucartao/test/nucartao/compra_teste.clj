ef427095814e405eb3513d61fb67bb17094de156
Camila Vargas <camila.vargas@camila.vargas>
2021-12-02 13:57:29 -0300

    add more tests

--- nucartao/test/nucartao/compra_teste.clj
+++ nucartao/test/nucartao/compra_teste.clj
@@ -32,23 +32,6 @@
                                       {:id 2, :cartao 50, :detalhes {:data "2020 10 02", :valor 10, :estabelecimento "EscolaABC", :categoria "Educação"}},
                                       {:id 3, :cartao 10, :detalhes {:data "2020 10 02", :valor 5, :estabelecimento "FarmaciaABC", :categoria "Saúde"}}]))))))
 
-
-;erro ao comparar o campo de data
-;(deftest nova-compra-detalhada-teste
-;  (testing "teste da funcao que adiciona nova compra"
-;    (is (= [{:id 1, :cartao 10, :detalhes {:data (data 2020 10 02), :valor 80, :estabelecimento "EscolaABC", :categoria "Educação"}},
-;            {:id 2, :cartao 50, :detalhes {:data (data 2020 10 05), :valor 10, :estabelecimento "EscolaABC", :categoria "Educação"}},
-;            {:id 3, :cartao 10, :detalhes {:data (data 2020 10 10), :valor 5, :estabelecimento "FarmaciaABC", :categoria "Saúde"}},
-;            {:id 4, :cartao 20, :detalhes {:data (data 2020 11 02), :valor 50, :estabelecimento "EscolaABC", :categoria "Educação"}},
-;            {:id 5, :cartao 20, :detalhes {:data (data 2020 11 30), :valor 50, :estabelecimento "EscolaABC", :categoria "Educação"}},
-;            {:id 6, :cartao 20, :detalhes {:data (data 2020 12 02), :valor 5, :estabelecimento "EscolaABC", :categoria "Educação"}},
-;            {:id 7, :cartao 30, :detalhes {:data (data 2020 12 02), :valor 30, :estabelecimento "SalaoABC", :categoria "Beleza"}},
-;            {:id 8, :cartao 30, :detalhes {:data (data 2020 12 31), :valor 40, :estabelecimento "EscolaABC", :categoria "Educação"}},
-;            {:id 9, :cartao 40, :detalhes {:data (data 2020 12 31), :valor 5, :estabelecimento "FarmaciaABC", :categoria "Saúde"}},
-;            {:cartao 10, :detalhes {:valor 180, :estabelecimento "FarmaciaABC", :categoria "Saude", :data data}, :id 10}]
-;              (nova-compra-detalhada {:cartao 10, :detalhes {:valor 180, :estabelecimento "FarmaciaABC", :categoria "Saude"}})))))
-
-
 (deftest todas-as-compras-por-categoria-teste
   (testing "Formato de retorno válido - teste da funcao de todos gastos por categoria"
     (is (= [{"Educação" "R$ 90.00"} {"Saúde" "R$ 5.00"}]
@@ -62,6 +45,19 @@
                                                  {:valor 10, :estabelecimento "EscolaABC", :categoria "Educação"},
                                                  {:valor 5, :estabelecimento "FarmaciaABC", :categoria "Saúde"}]))))))
 
+(deftest nova-compra-detalhada-teste
+  (testing "teste da funcao que adiciona nova compra"
+    (is (assert-any [{:id 1, :cartao 10, :detalhes {:data (data 2020 10 02), :valor 80, :estabelecimento "EscolaABC", :categoria "Educação"}},
+            {:id 2, :cartao 50, :detalhes {:data (data 2020 10 05), :valor 10, :estabelecimento "EscolaABC", :categoria "Educação"}},
+            {:id 3, :cartao 10, :detalhes {:data (data 2020 10 10), :valor 5, :estabelecimento "FarmaciaABC", :categoria "Saúde"}},
+            {:id 4, :cartao 20, :detalhes {:data (data 2020 11 02), :valor 50, :estabelecimento "EscolaABC", :categoria "Educação"}},
+            {:id 5, :cartao 20, :detalhes {:data (data 2020 11 30), :valor 50, :estabelecimento "EscolaABC", :categoria "Educação"}},
+            {:id 6, :cartao 20, :detalhes {:data (data 2020 12 02), :valor 5, :estabelecimento "EscolaABC", :categoria "Educação"}},
+            {:id 7, :cartao 30, :detalhes {:data (data 2020 12 02), :valor 30, :estabelecimento "SalaoABC", :categoria "Beleza"}},
+            {:id 8, :cartao 30, :detalhes {:data (data 2020 12 31), :valor 40, :estabelecimento "EscolaABC", :categoria "Educação"}},
+            {:id 9, :cartao 40, :detalhes {:data (data 2020 12 31), :valor 5, :estabelecimento "FarmaciaABC", :categoria "Saúde"}},
+            {:cartao 10, :detalhes {:valor 180, :estabelecimento "FarmaciaABC", :categoria "Saude", :data data}, :id 10}]
+              (nova-compra-detalhada {:cartao 10, :detalhes {:valor 180, :estabelecimento "FarmaciaABC", :categoria "Saude"}})))))
 
 (deftest detalhar-compras-do-cartao-teste
   (testing "Formato de retorno válido - teste da funcao que detalha todas as compras do cartao"
