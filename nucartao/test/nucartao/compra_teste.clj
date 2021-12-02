0f5c36fbdc8f965387c313adb306f698f247577e
Camila Vargas <camila.vargas@camila.vargas>
2021-12-02 11:04:26 -0300

    add more tests

--- nucartao/test/nucartao/compra_teste.clj
+++ nucartao/test/nucartao/compra_teste.clj
@@ -9,5 +9,14 @@
   (testing "Verdadeiro quando o valor é 1"
     (is (maior-ou-igual-a-zero? 1)))
 
-  (testing "Falso quando o velor é -1"
+  (testing "Falso quando o valor é -1"
     (is (not (maior-ou-igual-a-zero? -1)))))
+
+(deftest detalhes-de-compras-teste
+  (testing "Formato de retorno válido da funcao detalhes-de-compras"
+    (is (= [{:data "2020 10 02", :valor 80, :estabelecimento "EscolaABC", :categoria "Educação"}]
+           (detalhes-de-compras [{:id 1, :cartao 10, :detalhes {:data "2020 10 02", :valor 80, :estabelecimento "EscolaABC", :categoria "Educação"}}]))))
+
+  (testing "Formato de retorno inválido da funcao detalhes-de-compras"
+    (is (not (= [:detalhes {:data "2020 10 02", :valor 80, :estabelecimento "EscolaABC", :categoria "Educação"}]
+                (detalhes-de-compras [{:id 1, :cartao 10, :detalhes {:data "2020 10 02", :valor 80, :estabelecimento "EscolaABC", :categoria "Educação"}}]))))))
