4c8e0e7de8f6c0d307355432db22aa487426af19
Camila Vargas <camila.vargas@camila.vargas>
2021-12-02 10:36:00 -0300

    adjustment

--- nucartao/test/nucartao/compra_teste.clj
+++ nucartao/test/nucartao/compra_teste.clj
@@ -2,18 +2,12 @@
   (:require [clojure.test :refer :all]
             [nucartao.logica :refer :all]))
 
-(deftest maior-ou-igual-a-zero?-retornar-true-quando-o-numero-eh-0
-  (testing "Existe compra para o cartao indicado"
-    (is (maior-ou-igual-a-zero? 0))))
+(deftest maior-ou-igual-a-zero?-teste
+  (testing "Verdadeiro quando o valor é 0"
+    (is (maior-ou-igual-a-zero? 0)))
 
-(deftest maior-ou-igual-a-zero?-retornar-true-quando-o-numero-eh-1
-  (testing "Existe compra para o cartao indicado"
-    (is (maior-ou-igual-a-zero? 1))))
+  (testing "Verdadeiro quando o valor é 1"
+    (is (maior-ou-igual-a-zero? 1)))
 
-(deftest maior-ou-igual-a-zero?-retornar-false-quando-o-numero-eh-menor-do-que-zero
-  (testing "Existe compra para o cartao indicado"
+  (testing "Falso quando o velor é -1"
     (is (not (maior-ou-igual-a-zero? -1)))))
-
-(deftest maior-ou-igual-a-zero?-retornar-true-quando-o-numero-eh-0
-  (testing "Existe compra para o cartao indicado"
-    (is (maior-ou-igual-a-zero? 0))))
